a682d76d63b611b5da2401df0d8202e8e0734ce7
Luv <78132832+luv003@users.noreply.github.com>
2023-03-11 19:22:54 +0530

    Create import_dependencies.py

---
+++ import_dependencies.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os.path
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from sklearn.metrics import confusion_matrix
