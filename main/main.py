0c18279fe1d4ee52b96bcf762a1800fc7535b9db
Luv <78132832+luv003@users.noreply.github.com>
2023-03-13 13:59:31 +0530

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,13 +1,3 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import os.path
-from pathlib import Path
-from sklearn.model_selection import train_test_split
-import tensorflow as tf
-from sklearn.metrics import confusion_matrix
-
 image_dir=Path('./dataset/')
 filepaths= list(image_dir.glob(r'**/*.jpg'))
 
